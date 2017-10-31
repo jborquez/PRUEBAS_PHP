c4a393a04d3e02c05ac44982a2cb5ab15a476b7f
Jorge Bórquez R <jborquez@jbr-progap-deb.udec.cl>
2017-10-31 19:46:02 -0300

    Modifico en README e incporporo carpeta curso_html2pdf para pruebas

---
+++ curso_html2pdf/print_view.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Generar PDF con PHP</title>
+  <style type = "text/css">
+  </style>
+</head>
+<body>
+  <h1>Kiuz Loco! - Región de Ñuble</h1><h2>Nueva línea del archivo</h2>
+</body>
+</html>
