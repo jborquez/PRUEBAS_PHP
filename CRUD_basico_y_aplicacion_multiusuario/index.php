eca28852cf5de63921f5d8303a704e5714d3e3c3
Jorge Bórquez R <jorgeborquez@udec.cl>
2017-11-06 01:40:03 -0300

    Curso CRUD - UPDATE Correcto

--- CRUD_basico_y_aplicacion_multiusuario/index.php
+++ CRUD_basico_y_aplicacion_multiusuario/index.php
@@ -5,7 +5,7 @@ if (!empty($_POST)) {
   $codigo = mysqli_real_escape_string($conexion,$_POST['codigo']);
   $asignatura = mysqli_real_escape_string($conexion,$_POST['asignatura']);
   $nota = mysqli_real_escape_string($conexion,$_POST['nota']);
-  // ***** VERIFICAMOS SI LA SIGNATURA YA EXISTE EN LA BASE DE DATOS *****
+  // ***** VERIFICAMOS SI LA ASIGNATURA YA EXISTE EN LA BASE DE DATOS *****
   $verificarasignatura = "SELECT * FROM asignaturas WHERE codigoasignatura ='$codigo' OR nombreasignatura = '$asignatura'";
   $taonota = $conexion->query($verificarasignatura);
   $tuplas = $taonota->num_rows;
@@ -39,6 +39,8 @@ if (!empty($_POST)) {
     }
   }
 }
+$sql_lista_asignaturas = "SELECT * FROM asignaturas";
+$query_lista_asignturas = $conexion->query($sql_lista_asignaturas);
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -58,7 +60,7 @@ if (!empty($_POST)) {
   </form>
   <hr>
   <h4 align="center">*** Mis Asignaturas ***</h4>
-  <table border="1">
+  <table border="1" align="center">
     <thead>
       <tr>
         <th>Código</th>
@@ -69,11 +71,20 @@ if (!empty($_POST)) {
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td>CO100</td>
-        <td>Programación</td>
-        <td>100</td>
-      </tr>
+      <?php
+        while ($row_asignaturas = $query_lista_asignturas->fetch_array(MYSQLI_BOTH)) {
+          echo "
+          <tr>
+            <td>".$row_asignaturas['codigoasignatura']."</td>
+            <td>".$row_asignaturas['nombreasignatura']."</td>
+            <td>".$row_asignaturas['nota']."</td>
+            <td><a href='editar_asignatura.php?id_asignatura=".$row_asignaturas['id_asignatura']."'>Editar</a></td>
+
+          </tr>";
+
+        }
+        $conexion->close();
+      ?>
     </tbody>
   </table>
 </body>
